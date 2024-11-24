e6314d3206169310146127de0ce8c4055b855fc0
shdhkims@naver.com <49316394+shdhkim@users.noreply.github.com>
2024-11-24 14:59:46 +0900

    11/24 배포테스트

--- src/main/java/com/maumai/glasses/kiosk/GlassesKioskApplication.java
+++ src/main/java/com/maumai/glasses/kiosk/GlassesKioskApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GlassesKioskApplication {
 
 	public static void main(String[] args) {
-		  SpringApplication.run(GlassesKioskApplication.class, args);
+		   SpringApplication.run(GlassesKioskApplication.class, args);
 	}
 
 }
