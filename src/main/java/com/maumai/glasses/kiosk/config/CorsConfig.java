bba36e5fee312c227c60a6029f4bfb242bca3986
shdhkims@naver.com <49316394+shdhkim@users.noreply.github.com>
2024-11-27 14:50:06 +0900

    11/25 config 수정

--- src/main/java/com/maumai/glasses/kiosk/config/CorsConfig.java
+++ src/main/java/com/maumai/glasses/kiosk/config/CorsConfig.java
@@ -11,15 +11,16 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
-
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
 
-        config.addAllowedHeader("Authorization");
-        config.addAllowedMethod("*");
-
-        config.addAllowedOrigin("*");
-        config.setAllowCredentials(true);
+        // 허용할 헤더, 메서드, 도메인 설정
+        config.addAllowedHeader("*"); // 모든 헤더 허용
+        config.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+        config.addAllowedOrigin("http://localhost:3000"); // 로컬 개발 환경 허용
+        config.addAllowedOrigin("http://127.0.0.1:3000"); // IP 형식 로컬호스트 허용
+        config.addAllowedOrigin("https://your-cloudfront-domain.com"); // 배포된 도메인 허용
+        config.setAllowCredentials(true); // 쿠키 및 인증 정보 허용
 
         source.registerCorsConfiguration("/**", config);
 
