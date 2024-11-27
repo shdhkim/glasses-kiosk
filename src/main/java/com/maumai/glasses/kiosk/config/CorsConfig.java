b59d8eea0cb3cdbf05681dde89f142c01943c50a
shdhkims@naver.com <49316394+shdhkim@users.noreply.github.com>
2024-11-27 19:16:05 +0900

    no message

--- src/main/java/com/maumai/glasses/kiosk/config/CorsConfig.java
+++ src/main/java/com/maumai/glasses/kiosk/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
         config.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
         config.addAllowedOrigin("http://localhost:3000"); // 로컬 개발 환경 허용
         config.addAllowedOrigin("http://127.0.0.1:3000"); // IP 형식 로컬호스트 허용
-        config.addAllowedOrigin("https://your-cloudfront-domain.com"); // 배포된 도메인 허용
+        config.addAllowedOrigin("https://d9hbd26otiifc.cloudfront.net"); // 배포된 도메인 허용
         config.setAllowCredentials(true); // 쿠키 및 인증 정보 허용
 
         source.registerCorsConfiguration("/**", config);
