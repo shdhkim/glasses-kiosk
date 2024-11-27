8db01940e9a7af673d88b2b12f608e8f379261d4
shdhkims@naver.com <49316394+shdhkim@users.noreply.github.com>
2024-11-27 20:48:30 +0900

    no message

--- src/main/java/com/maumai/glasses/kiosk/GlassesKioskApplication.java
+++ src/main/java/com/maumai/glasses/kiosk/GlassesKioskApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GlassesKioskApplication {
 
-	public static void main(String[] args) {   SpringApplication.run(GlassesKioskApplication.class, args);
+	public static void main(String[] args) {  SpringApplication.run(GlassesKioskApplication.class, args);
 	}
 
 }
